2007dae9ba01564bc7bf6a442d5aed7c5478db49
Radu-A <elcastravete@gmail.com>
2025-12-20 12:48:47 +0100

    montando-arbol acabado

--- 25-12-19_montando-arbol.js
+++ 25-12-19_montando-arbol.js
@@ -4,34 +4,28 @@
  *  @returns {string} The decorated tree
  */
 
-const array = [
-  ["    *    "],
-  ["   ***   "],
-  ["  *****  "],
-  [" ******* "],
-  ["*********"],
-  ["    #    "],
-];
-
 function drawTree(height, ornament, frequency) {
   const lineLength = 1 + 2 * (height - 1);
   const half = Math.floor(lineLength / 2);
   let tree = "";
+  let count = 1;
   for (let i = 0; i < height; i++) {
-    const whiteSpaces = half - i;
+    const whiteSpaces = " ".repeat(half - i);
     const leafs = 1 + i * 2;
-    const line =
-      " ".repeat(whiteSpaces) +
-      "*".repeat(leafs) +
-      " ".repeat(whiteSpaces) +
-      "\n";
-    tree = tree + line;
+    let pattern = "";
+    for (let j = 0; j < leafs; j++) {
+      if (count % frequency !== 0) {
+        pattern += "*";
+      } else {
+        pattern += ornament;
+      }
+      count++;
+    }
+    tree += whiteSpaces + pattern + "\n";
   }
-  const trunk = " ".repeat(half) + "#" + " ".repeat(half) + "\n";
+  const trunk = " ".repeat(half) + "#";
   tree += trunk;
-  console.log(tree);
-
-  return "";
+  return tree;
 }
 
 drawTree(5, "o", 2);
