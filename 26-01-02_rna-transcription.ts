36bbe5efa349eaf81aa556a6810d91dacd2c34f2
Radu-A <elcastravete@gmail.com>
2026-01-02 11:10:20 +0100

    working rna transcription -> refactorgit add .git add .

---
+++ 26-01-02_rna-transcription.ts
@@ -0,0 +1,38 @@
+// Your task is to determine the RNA complement of a given DNA sequence.
+
+// Both DNA and RNA strands are a sequence of nucleotides.
+
+// The four nucleotides found in DNA are adenine (A), cytosine (C), guanine (G), and thymine (T).
+
+// The four nucleotides found in RNA are adenine (A), cytosine (C), guanine (G), and uracil (U).
+
+// Given a DNA strand, its transcribed RNA strand is formed by replacing each nucleotide with its complement:
+
+// G -> C
+// C -> G
+// T -> A
+// A -> U
+
+function toRna(dna: string): string {
+  type DNA = "G" | "C" | "T" | "A";
+  type RNA = "C" | "G" | "A" | "A";
+  const transcriptor: Record<string, string> = {
+    G: "C",
+    C: "G",
+    T: "A",
+    A: "U",
+  };
+  let rna: string = "";
+  for (const nucleotide of dna) {
+    if (nucleotide in transcriptor) {
+      // console.log("Fuck: Valid input DNA.");
+      rna = rna + transcriptor[nucleotide];
+    } else {
+      return "Invalid input DNA.";
+    }
+  }
+  return rna;
+}
+
+console.log(toRna("ACGTGGTCTTAA"));
+console.log(toRna("ACGTXXXCTTAA"));
