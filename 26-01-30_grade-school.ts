c271d6d58abf7745bdff351c28443c831030635a
Radu-A <elcastravete@gmail.com>
2026-02-04 11:56:44 +0100

    grade-school: ROSTER CANNOT BE MODIFIED OUTSIDE OF MODULE

--- 26-01-30_grade-school.ts
+++ 26-01-30_grade-school.ts
@@ -13,7 +13,7 @@
 // "Let me think. We have Anna, Barb, and Charlie in grade 1, Alex, Peter, and Zoe in grade 2, and Jim in grade 5. So the answer is: Anna, Barb, Charlie, Alex, Peter, Zoe, and Jim."
 // Note that all our students only have one name (it's a small town, what do you want?), and each student cannot be added more than once to a grade or the roster. If a test attempts to add the same student more than once, your implementation should indicate that this is incorrect.
 
-type MyGrades = "1" | "2" | "3" | "4" | "5" | "6";
+// type MyGrades = "1" | "2" | "3" | "4" | "5" | "6";
 
 // interface rosterInterface {
 //    [keyof: string]: string[]
@@ -22,14 +22,12 @@ type MyGrades = "1" | "2" | "3" | "4" | "5" | "6";
 type rosterData = Record<string, string[]>;
 
 class GradeSchool {
-  gradeList?: {[keyof: string]: string[]} = {}
+  private gradeList?: { [keyof: string]: string[] } = {};
+  readonly publicGradeList = this.gradeList;
 
+  // ** ROSTER CANNOT BE MODIFIED OUTSIDE OF MODULE **
   roster() {
-    let sorted: {[keyof: string]: string[]} = {};
-    for (let grade in this.gradeList) {
-      const gradeSorted = this.gradeList[grade].sort();
-      sorted = { ...sorted, gradeSorted };
-    }
+    return this.publicGradeList;
   }
   add(name: string, grade: number) {
     if (!this.gradeList) {
@@ -41,16 +39,34 @@ class GradeSchool {
         this.gradeList = { ...this.gradeList, [grade]: [name] };
       }
     }
+    let sorted: { [keyof: string]: string[] } = {};
+    for (let grade in this.gradeList) {
+      const gradeSorted: string[] = this.gradeList[grade].sort();
+      this.gradeList = { ...this.gradeList, [grade]: gradeSorted };
+    }
   }
   grade(grade: number) {
     if (this.gradeList && this.gradeList[grade]) return this.gradeList[grade];
-    return {};
+    return [];
   }
 }
 const set = new Set();
 
 const school = new GradeSchool();
-school.add("Franklin", 5);
-school.add("Bradley", 5);
-school.add("Jeff", 1);
+// console.log(school.roster());
+// school.add("Franklin", 5);
+// school.add("Bradley", 5);
+// school.add("Jeff", 1);
+school.add("Aimee", 2);
+const roster = school.roster();
 console.log(school.roster());
+
+if (roster) {
+  try {
+    roster[2].push("Oops.");
+  } catch {
+    /* empty */
+  }
+}
+console.log(school.roster());
+// console.log(school.grade(5));
