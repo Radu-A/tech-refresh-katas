9481affa856c8d44878d8864a3e37f439601afde
Radu-A <elcastravete@gmail.com>
2026-02-15 17:53:32 +0100

    clock: coding

--- 26-02-10_clock.ts
+++ 26-02-10_clock.ts
@@ -6,35 +6,59 @@
 
 class Clock {
   total = 0;
+  hours = 0;
+  minutes = 0;
   constructor(hour: number, minute?: number) {
-    if (!minute) {
-      this.total = Math.floor(hour * 60);
-    } else {
-      this.total = hour * 60 + minute;
+    this.total = Math.floor(hour * 60);
+    if (minute) {
+      this.total = this.total + minute;
     }
+
+    this.hours = this.getHour();
+
+    this.minutes = this.getMinutes();
   }
 
-  public toString(): unknown {
-    const hourString = (this.total / 60).toString().padStart(2, "0");
-    const minuteString = (this.total % 60).toString().padStart(2, "0");
+  private getHour(total = this.total): number {
+    let hour = Math.floor(total / 60);
+    const days = Math.floor(hour / 24);
+    return hour - days * 24;
+  }
+
+  private getMinutes(total = this.total): number {
+    let minutes = total % 60;
+    if (this.total < 0) {
+      minutes = 60 + minutes;
+    }
+    return minutes;
+  }
+
+  public toString(): string {
+    const hour = this.getHour();
+    const hourString = hour.toString().padStart(2, "0");
+    const minutes = this.getMinutes();
+    const minuteString = minutes.toString().padStart(2, "0");
     return `${hourString}:${minuteString}`;
   }
 
-  // public plus(minutes?: unknown): Clock {
-  //   throw new Error("Remove this line and implement the function");
-  // }
+  public plus(minutes: number) {
+    this.total = this.total + minutes;
+  }
 
-  // public minus(minutes?: unknown): Clock {
-  //   throw new Error("Remove this line and implement the function");
-  // }
+  public minus(minutes: number) {
+    this.total = this.total - minutes;
+  }
 
   // public equals(other?: unknown): unknown {
   //   throw new Error("Remove this line and implement the function");
   // }
 }
 
-const clock = new Clock(8, 30);
-console.log(clock.toString());
+// const clock1 = new Clock(8, 30);
+// const clock2 = new Clock(0, 160);
+const clock3 = new Clock(0, 45);
+clock3.plus(40);
+console.log(clock3.toString());
 
 // =========
 // KEY LOGIC
