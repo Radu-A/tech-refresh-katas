45273462357f98507f2709aaf90a214eb47330d2
Radu-A <elcastravete@gmail.com>
2026-03-05 17:28:22 +0100

    finished:clocks

--- 26-02-10_clock.ts
+++ 26-02-10_clock.ts
@@ -5,21 +5,15 @@
 // Two clocks that represent the same time should be equal to each other.
 
 class Clock {
-  total = 0;
-  hours = 0;
-  minutes = 0;
+  private total = 0;
   constructor(hour: number, minute?: number) {
-    this.total = Math.floor(hour * 60);
+    this.total = hour * 60;
     if (minute) {
       this.total = this.total + minute;
     }
-
-    this.hours = this.getHour();
-
-    this.minutes = this.getMinutes();
   }
 
-  private getHour(total = this.total): number {
+  private getHours(total = this.total): number {
     let hour = Math.floor(total / 60);
     const days = Math.floor(hour / 24);
     return hour - days * 24;
@@ -27,38 +21,57 @@ class Clock {
 
   private getMinutes(total = this.total): number {
     let minutes = total % 60;
-    if (this.total < 0) {
+    if (this.total < 0 && minutes !== 0) {
       minutes = 60 + minutes;
     }
     return minutes;
   }
 
   public toString(): string {
-    const hour = this.getHour();
-    const hourString = hour.toString().padStart(2, "0");
+    const hours = this.getHours();
+    const hourString = hours.toString().padStart(2, "0");
     const minutes = this.getMinutes();
     const minuteString = minutes.toString().padStart(2, "0");
     return `${hourString}:${minuteString}`;
   }
 
   public plus(minutes: number) {
-    this.total = this.total + minutes;
+    this.total += minutes;
+    return this.toString();
   }
 
   public minus(minutes: number) {
-    this.total = this.total - minutes;
+    this.total -= minutes;
+    return this.toString();
   }
 
-  // public equals(other?: unknown): unknown {
-  //   throw new Error("Remove this line and implement the function");
-  // }
+  public equals(other: Clock) {
+    return this.toString() === other.toString();
+  }
 }
 
 // const clock1 = new Clock(8, 30);
 // const clock2 = new Clock(0, 160);
-const clock3 = new Clock(0, 45);
-clock3.plus(40);
-console.log(clock3.toString());
+
+// const clock3 = new Clock(0, 45);
+// clock3.plus(40);
+// console.log(clock3.toString());
+
+// const clock4 = new Clock(-25, 0);
+// console.log(clock4.toString());
+
+// console.log(new Clock(10, 0).plus(3).toString());
+
+// const clock5 = new Clock(8);
+// console.log(clock5.toString());
+
+// console.log(new Clock(0, 45).plus(40).toString());
+
+// console.log(new Clock(15, 37).equals(new Clock(15, 37)));
+
+const time = (1 * 60 + 60) % 1440;
+
+console.log(time);
 
 // =========
 // KEY LOGIC
